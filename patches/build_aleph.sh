c529eddc72fd315d8c13ad78ddabcfdc6a0dea60
Markus Frank <Markus.Frank@cern.ch>
2026-08-12 18:46:45 +0200

    Use CERNLIB from git@gitlab.cern.ch:7999/DPHEP/cernlib

--- patches/build_aleph.sh
+++ patches/build_aleph.sh
@@ -15,9 +15,9 @@ exec_echo()  {
 }
 #
 # ==================================================================================================
-do_checkout_package()  {
+aleph-do-checkout_package()  {
     curr=`pwd`;
-    cd ${BUILD_DIR};
+    cd ${ALEPH_BUILD_DIR};
     git_dir=${1}
     repo=${2};
     if test -d ${repo}; then
@@ -26,61 +26,64 @@ do_checkout_package()  {
         cd -;
     else
         git clone ${git_dir}/${repo}.git;
+        cd ${repo};
+        git fetch    origin cmake-build;
+        git checkout origin/cmake-build;
+        cd -;
     fi;
     cd ${curr};
 }
 #
 #
 # ==================================================================================================
-do-checkout()  {
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/DPHEP/cernlib  cernlib;
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software inc;
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software bos77;
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software alephlib
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software dbase
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software alephio
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software alpha
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software julia
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software galeph
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software look
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software mini
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software tpcsim
-    do_checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software uphy
+aleph-do-checkout()  {
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software inc;
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software bos77;
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software alephlib
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software dbase
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software alephio
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software alpha
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software julia
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software galeph
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software look
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software mini
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software tpcsim
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/aleph/software uphy
 }
 #
 #
 # ==================================================================================================
 patch-checkout()   {
     curr=`pwd`;
-    cd ${BUILD_DIR};
-    if test -f ${BUILD_DIR}/alephlib/ldes/lcalmv.F; then
-        mv ${BUILD_DIR}/alephlib/ldes/lcalmv.F ${BUILD_DIR}/alephlib/ldes/lcalmv.F.exclude;
+    cd ${ALEPH_BUILD_DIR};
+    if test -f ${ALEPH_BUILD_DIR}/alephlib/ldes/lcalmv.F; then
+        mv ${ALEPH_BUILD_DIR}/alephlib/ldes/lcalmv.F ${ALEPH_BUILD_DIR}/alephlib/ldes/lcalmv.F.exclude;
     fi;
-    if test -f ${BUILD_DIR}/alephlib/gam/intif4.F; then
-        mv ${BUILD_DIR}/alephlib/gam/intif4.F ${BUILD_DIR}/alephlib/ldes/intif4.F.exclude;
+    if test -f ${ALEPH_BUILD_DIR}/alephlib/gam/intif4.F; then
+        mv ${ALEPH_BUILD_DIR}/alephlib/gam/intif4.F ${ALEPH_BUILD_DIR}/alephlib/ldes/intif4.F.exclude;
     fi;
-    if test -f ${BUILD_DIR}/julia/l_/lbdlba.F; then
-        mv ${BUILD_DIR}/julia/l_/lbdlba.F ${BUILD_DIR}/julia/l_/lbdlba.F.exclude;
+    if test -f ${ALEPH_BUILD_DIR}/julia/l_/lbdlba.F; then
+        mv ${ALEPH_BUILD_DIR}/julia/l_/lbdlba.F ${ALEPH_BUILD_DIR}/julia/l_/lbdlba.F.exclude;
     fi;
-    if test -f ${BUILD_DIR}/tpcsim/F77/tpmain.F; then
-        mv ${BUILD_DIR}/tpcsim/F77/tpmain.F ${BUILD_DIR}/tpcsim/F77/tpmain.F.exclude;
+    if test -f ${ALEPH_BUILD_DIR}/tpcsim/F77/tpmain.F; then
+        mv ${ALEPH_BUILD_DIR}/tpcsim/F77/tpmain.F ${ALEPH_BUILD_DIR}/tpcsim/F77/tpmain.F.exclude;
     fi;
-    if test -f ${BUILD_DIR}/galeph/draw/guinti.F; then
-        mv ${BUILD_DIR}/galeph/draw/guinti.F ${BUILD_DIR}/galeph/draw/guinti.F.exclude;
+    if test -f ${ALEPH_BUILD_DIR}/galeph/draw/guinti.F; then
+        mv ${ALEPH_BUILD_DIR}/galeph/draw/guinti.F ${ALEPH_BUILD_DIR}/galeph/draw/guinti.F.exclude;
     fi;
-    if test -f ${BUILD_DIR}/galeph/vdet/agvdet.F; then
-        mv ${BUILD_DIR}/galeph/vdet/agvdet.F ${BUILD_DIR}/galeph/vdet/agvdet.F.exclude;
+    if test -f ${ALEPH_BUILD_DIR}/galeph/vdet/agvdet.F; then
+        mv ${ALEPH_BUILD_DIR}/galeph/vdet/agvdet.F ${ALEPH_BUILD_DIR}/galeph/vdet/agvdet.F.exclude;
     fi;
-    if test -f ${BUILD_DIR}/alpha/pack/qfget_bp.F; then
-        mv ${BUILD_DIR}/alpha/pack/qfget_bp.F ${BUILD_DIR}/alpha/pack/qfget_bp.F.exclude;
+    if test -f ${ALEPH_BUILD_DIR}/alpha/pack/qfget_bp.F; then
+        mv ${ALEPH_BUILD_DIR}/alpha/pack/qfget_bp.F ${ALEPH_BUILD_DIR}/alpha/pack/qfget_bp.F.exclude;
     fi;
-    if test -f ${BUILD_DIR}/alpha/qfn/fit_dmin.F; then
-        mv ${BUILD_DIR}/alpha/qfn/fit_dmin.F ${BUILD_DIR}/alpha/qfn/fit_dmin.F.exclude;
+    if test -f ${ALEPH_BUILD_DIR}/alpha/qfn/fit_dmin.F; then
+        mv ${ALEPH_BUILD_DIR}/alpha/qfn/fit_dmin.F ${ALEPH_BUILD_DIR}/alpha/qfn/fit_dmin.F.exclude;
     fi;
-    if test -f ${BUILD_DIR}/alpha/qfn/btag_fit.F; then
-        mv ${BUILD_DIR}/alpha/qfn/btag_fit.F ${BUILD_DIR}/alpha/qfn/btag_fit.F.exclude;
+    if test -f ${ALEPH_BUILD_DIR}/alpha/qfn/btag_fit.F; then
+        mv ${ALEPH_BUILD_DIR}/alpha/qfn/btag_fit.F ${ALEPH_BUILD_DIR}/alpha/qfn/btag_fit.F.exclude;
     fi;
-    exec_echo cp -r ${ALEPH}/aleph-offline/patches/* ${BUILD_DIR}/;
+    exec_echo cp -r ${ALEPH}/aleph-offline/patches/* ${ALEPH_BUILD_DIR}/;
     cd ${curr};
 }
 #
@@ -88,20 +91,16 @@ patch-checkout()   {
 # ==================================================================================================
 verify-checkout()  {
     curr=`pwd`;
-    cd ${BUILD_DIR};
-    if test -d ./cernlib; then
-	echo "" > /dev/null;
-    elif test -d ../cernlib; then
-	cd ..;
-    fi;
+    cd ${ALEPH_BUILD_DIR};
     echo "+++ Check software base at `pwd`";
-    for i in alephio alephlib alpha bos77 dbase galeph inc julia look mini tpcsim uphy;
-    do
+    for i in alephio alephlib alpha bos77 dbase galeph inc julia look mini tpcsim uphy; do
         cd $i;
         printf "================================= %s \n" "`pwd`";
+        #rm -rf $i;
         git status;
-	git commit -a -m "Use CERNLIB from git@gitlab.cern.ch:7999/DPHEP/cernlib";
-	git push origin cmake-build;
+        #git checkout -b cmake-build;
+	#git commit -a -m "Use CERNLIB from git@gitlab.cern.ch:7999/DPHEP/cernlib";
+	#git push -f origin cmake-build;
         cd ..;
     done;
     cd ${curr};
@@ -110,21 +109,24 @@ verify-checkout()  {
 #
 install-cernlib()  {
     curr=`pwd`;
-    cernlib_dir=${BUILD_DIR}/cernlib;
+    cernlib_dir=${ALEPH_BUILD_DIR}/cernlib;
+    #
+    cd ${ALEPH_BUILD_DIR};
+    aleph-do-checkout_package ssh://git@gitlab.cern.ch:7999/DPHEP/cernlib cernlib;
     mkdir -p ${cernlib_dir}/build64;
     cd ${cernlib_dir}/build64;
     #
-    echo "+++ may need: sudo apt-get install libxaw7-dev ";
+    echo "+++ may need: sudo apt-get install libxaw7-dev";
     echo "+++ may need: sudo apt-get install libnsl2";
     #
     #
     exec_echo \
         cmake -DCMAKE_INSTALL_PREFIX=${cernlib_dir}/install64 \
-          -DCERNLIB_BUILD_SHARED=ON \
-          -DCERNLIB_POSITION_INDEPENDENT_CODE=ON \
-          -DCERNLIB_USE_INTERNAL_LAPACK=ON \
-          -DCERNLIB_2022=ON \
-          -DLINUX=ON ..;
+        -DCERNLIB_BUILD_SHARED=ON \
+        -DCERNLIB_POSITION_INDEPENDENT_CODE=ON \
+        -DCERNLIB_USE_INTERNAL_LAPACK=ON \
+        -DCERNLIB_2022=ON \
+        -DLINUX=ON ..;
     make -j 33 install;
     cd ${curr};
 }
@@ -132,49 +134,80 @@ install-cernlib()  {
 #
 gen-headers-aleph()  {
     curr=`pwd`;
-    mkdir -p ${BUILD_DIR}/build64;
-    cd ${BUILD_DIR}/build64;
+    mkdir -p ${ALEPH_BUILD_DIR}/build64;
+    cd ${ALEPH_BUILD_DIR}/build64;
     python ../dbase/scripts/gen_header.py -o ../aleph_headers/alpha -a;
     cd ${curr};
 }
+# ==================================================================================================
 #
 install-aleph()  {
     curr=`pwd`;
-    mkdir -p ${BUILD_DIR}/build64;
-    cd ${BUILD_DIR}/build64;
+    mkdir -p ${ALEPH_BUILD_DIR}/build64;
+    cd ${ALEPH_BUILD_DIR}/build64;
     #
+    install=`pwd`/../install64;
     exec_echo \
-    cmake  -DCMAKE_MODULE_PATH=/cvmfs/sft.cern.ch/lcg/views/LCG_110/x86_64-ubuntu2404-gcc13-opt\
-	   -DCERNLIB_DIR=${BUILD_DIR}/cernlib/install64/share/cernlib/cmake \
-	   -DCMAKE_INSTALL_PREFIX=`pwd`/../install64 \
+    cmake  -DCMAKE_MODULE_PATH=${LCG_VIEW}                  \
+	   -DCERNLIB_DIR=${CERNLIB_DIR}/share/cernlib/cmake \
+	   -DCMAKE_INSTALL_PREFIX=`realpath ${install}`     \
 	   ..;
     make -j 33 install;
     cd ${curr};
 }
 # ==================================================================================================
 #
-export BUILD_DIR=${ALEPH}/gitlab;
 build-aleph()  {
-    mkdir -p ${BUILD_DIR};
+    mkdir -p ${ALEPH_BUILD_DIR};
     #
-    do-checkout;
+    aleph-do-checkout;
     #
     patch-checkout;
     #
-    . /cvmfs/sft.cern.ch/lcg/views/LCG_110/x86_64-ubuntu2404-gcc13-opt/setup.sh;
+    . ${LCG_VIEW}/setup.sh;
     #
     #
-    install-cernlib;
+    # install-cernlib;
     #
     gen-headers-aleph;
     #
     install-aleph;
     #
     #
-    cd ${BUILD_DIR}/build64;
+    cd ${ALEPH_BUILD_DIR}/build64;
     ./alpha/alpha.124;
 }
-
 # ==================================================================================================
-echo "+++ type build-aleph to build and checkout in the current working directory.";
+aleph-pathes()  {
+    . ${CERNLIB_DIR}/setup.sh;
+    if test -z "${LD_LIBRARY_PATH}"; then
+	export LD_LIBRARY_PATH=${ALEPH_BUILD_DIR}/install64/lib:${CERNLIB_DIR}/lib;
+    else
+	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ALEPH_BUILD_DIR}/install64/lib:${CERNLIB_DIR}/lib;
+    fi;
+    #
+    if test -z "${PATH}"; then
+	export PATH=${ALEPH_BUILD_DIR}/install64/lib:${CERNLIB_DIR}/bin;
+    else
+	export PATH=${PATH}:${ALEPH_BUILD_DIR}/install64/bin:${CERNLIB_DIR}/bin;
+    fi;
+}
+# ==================================================================================================
+aleph-info()  {
+    echo "+++ LCG_VIEW:              ${LCG_VIEW}";
+    echo "+++ CERNLIB_DIR:           ${CERNLIB_DIR}";
+    echo "+++ ALEPH_BUILD_DIR:       ${ALEPH_BUILD_DIR}";
+    echo "+++ type build-aleph       to build and checkout in the current working directory.";
+    echo "+++      install-cernlib   to build cernlib only.";
+    echo "+++      install-aleph     to build aleph stuff only.";
+    echo "+++      aleph-pathes      to set PATH and LD_LIBRARY_PATH";
+    echo "+++      patch-checkout    to patch checkouts";
+    echo "+++      gen-headers-aleph to generate header files corresponding to BOS banks";
+}
+# ==================================================================================================
+. /home/frankm/Aleph/offline/setaleph.sh;
+export LCG_VIEW=/cvmfs/sft.cern.ch/lcg/views/LCG_110/x86_64-ubuntu2404-gcc13-opt;
+export CERNLIB_DIR=/cvmfs/dphep.cern.ch/cernlib/releases/ubuntu-24-x86_64/cm/std/gcc/new;
+export ALEPH_BUILD_DIR=${ALEPH}/gitlab;
+aleph-info;
 # ==================================================================================================
---
+++ patches/cxx/opt/chkloc.c
@@ -0,0 +1,158 @@
+/*
+ *
+ * Revision 1.1.1.1  2006/06/14
+ * Kernlib  utility for locf, locb on LP64 architectures  H. Vogt
+ * (AMD64/Intel EM64T and IA64) 
+ *
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+/*>    ROUTINE LOCF
+  CERN PROGLIB# N100    LOCF            .VERSION KERNFOR  4.36  930602
+*/
+/*>    ROUTINE LOCB
+  CERN PROGLIB# N101    LOCB            .VERSION KERNFOR  4.36  930602
+*/
+
+unsigned int chkloc(iadr)
+   char *iadr;
+{
+  /* 64 bit architectures may exceed the 32 bit address space !               */
+
+  /* AMD64/Intel EM64T architectures have the dynamic segments above
+     0x80000000000 and the stack immediately below this whereas the text and
+     data segments are staring from 0x400000. The implementations address
+     space is limited to 0x00007fffffffffff.
+     Allocated memory with malloc/calloc is starting from the end of text and
+     data segments upwards.                                                   
+
+     IA64 architectures have the dynamic segments are above 0x2000000000000000,
+     the stack is above 0x8000000000000000, the data segments starts at 
+     0x6000000000000000 and the text segments start at 0x4000000000000000   
+     Allocated memory with malloc/calloc is starting from the end of
+     data segments upwards. All addresses here are expected to be in
+     the data segment area.                                                   */
+
+  /* K. McCarty: On Alpha, on the other hand, I couldn't find any docs for the
+     Linux segmentation for virtual memory.  Judging by a test machine
+     (escher.debian.org), the data section starts slightly above 0x120010000.
+     On the other hand the stack grows downward from 0x120000000. */
+  /* U. Schwickerath on aarch64 the stack grows downward as for Alpha */
+
+   const unsigned long mask=0xffffffff00000000;
+   static unsigned long limit=0x00000000ffffffff;
+   unsigned long jadr=((unsigned long) iadr & mask);
+#if defined (__ia64__)
+    if ( jadr != 0x6000000000000000) {
+#elif defined (__alpha__)
+    if ( jadr != 0x0000000100000000) {
+#elif  defined(__powerpc__)
+    if ( jadr != 0x0000000100000000) {
+      fprintf(stderr, "ppc64\n");
+#elif  defined(__DARWIN__)
+    if ( jadr != 0x0000000100000000) {
+      fprintf(stderr, "darwin\n");
+#else  /* amd64, aarch64, arm64 */
+
+    /*
+      In linux the common blocks and all other static data
+      are located in the .bss data segment of the executable.
+      We offset the start address of each data segment found
+      from the address given.
+      The data segments are defined in /proc/self/maps)
+      and have no underlying file in sections like this:
+      "
+      555555554000-55555555b000 r--p 00000000 103:04 13529449                  <exe>
+      55555555b000-555555783000 r-xp 00007000 103:04 13529449                  <exe>
+      555555783000-5555557a8000 r--p 0022f000 103:04 13529449                  <exe>
+      5555557a8000-5555557a9000 r--p 00254000 103:04 13529449                  <exe>
+      5555557a9000-5555557b7000 rw-p 00255000 103:04 13529449                  <exe>
+      5555557b7000-555556d47000 rw-p 00000000 00:00 0                   <<<<<< .bss
+      555556d47000-555556d89000 rw-p 00000000 00:00 0                          [heap]
+      7ffff6800000-7ffff6886000 r--p 00000000 00:40 746674                     <lib-1>
+      ...
+      7ffff6d96000-7ffff6e08000 rw-p 00000000 00:00 0                   <<<<<< data
+      7ffff7000000-7ffff709d000 r--p 00000000 103:01 1442019                   <lib-2>
+      ...
+      7ffff6d96000-7ffff6e08000 rw-p 00000000 00:00 0                   <<<<<< data
+
+      ....
+      "
+      So: we only check if the address is contained in one of the
+      data sections. If yes, return the offset to the beginning
+      of the section and off we go....
+
+      But:
+      This would have been nice, but is only half of the story:
+      In Fortran the data are in the .bss section, which is the
+      first data section after the main program. If all libraries
+      are shared, the data sections are intermittend between the
+      libraries as they are loaded. Then the relative offsets
+      lead to trouble, because depending on the section a common block
+      is in, data with the same offset may be in different sections.
+
+      This means:
+      The executable MUST be statically linked with all the Fortran code.
+      C++ code can be hosted in dynamically linked libraries as long
+      as the symbols from the main program are shared.
+      Hence this function will work if the common block is located
+      in the .bss section.
+
+      To be investigated:
+      It may be sufficient to check if the offset of the checked address
+      from the beginning of the executable section (the very first
+      entry in /proc/self/maps) is less than 2GB ( as for -m32! ).
+      Up to 2GB then also storage in shared libraries would be
+      considered.
+      Bad addresses may then result in a SIGBUS, but are valid address
+      space. This very likely would be way better than now simply
+      exiting.
+
+      M.Frank EP/LCB-CS
+    */
+    static uint64_t  base_address = 0;
+    if( 0 == base_address )  {
+       ssize_t  len = 0;
+       size_t   sl = 0;
+       char*    text = 0;
+       FILE*    in = fopen("/proc/self/maps", "r");
+
+       for( len=0, sl=0; (len=getline(&text, &sl, in)) != -1; )  {
+          uint64_t start=0, end=0;
+          if( 2 != sscanf(text, "%lx-%lx ", &start, &end) )  {
+             perror("Failed to get address of data segment.");
+             exit(EINVAL);
+          }
+          base_address = start;
+          break;
+      }
+      close(in);
+      if( text ) free(text);
+      printf("+++ Found base offset 0x%016x for data segments. "
+             "LOCF checks if address is with data segments.\n", base_address);
+      fflush(stdout);
+   }
+   iadr -= base_address;
+   jadr  = ((unsigned long) iadr & mask);
+   if ( jadr != 0) {
+      fprintf(stderr, "amd64, ppc64 or aarch64\n");
+#endif
+      fprintf(stderr, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+      fprintf(stderr, "LOCB/LOCF: address %p exceeds the 32 bit address space\n", iadr);
+      fprintf(stderr, "or is not in the data segments\n");
+      fprintf(stderr, "This may result in program crash or incorrect results\n");
+      fprintf(stderr, "Therefore we will stop here\n");
+      fprintf(stderr, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+      exit (999);
+    }
+    jadr=((unsigned long) iadr & limit);
+    return ((unsigned) jadr);
+}
+/*> END <----------------------------------------------------------*/
+
+    float etime_()  {  return 0e0; }
